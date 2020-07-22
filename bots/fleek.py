d5cdd4e387404357ad43f1ae6045a7df5f21558e
Ryan <44101248+Aqyl123@users.noreply.github.com>
2020-07-22 16:46:05 -0500

    Add files via upload

---
+++ bots/fleek.py
@@ -0,0 +1,22 @@
+import requests
+
+url = 'https://dev.botmart.io/api/product/fleek14065'
+response = requests.get(url)
+response.raise_for_status()
+result = response.json()
+
+name = result["data"]["product"]["name"]
+picture = result["data"]["product"]["thumbnails"]
+retail = result["data"]["product"]["retail"]
+website = result["data"]["product"]["website"]
+lowestask = result["data"]["lowest_ask_renewal"]
+description = result["data"]["product"]["description"]
+twitter = result["data"]["product"]["social_media"]["twitter"]
+supports = result["data"]["product"]["tags"]
+
+url2 = 'https://dev.botmart.io/api/product/7/lowest-ask?require_renewal=False&plan=Lifetime'
+response = requests.get(url2)
+response.raise_for_status()
+result2 = response.json()
+
+lifetime = result2["data"]["price"]
